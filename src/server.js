4f11079cea08f8a23f71eebda2bf56ac365d8099
João <pintojr@icomp.ufam.edu.br>
2019-10-03 08:04:00 -0400

    Configured cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@aircnc-eimhg.mongodb.net/aircnc?re
     useUnifiedTopology : true
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
