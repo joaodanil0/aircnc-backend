f9f6da8b0b58806ae47266246c9095fda0e9c542
João <pintojr@icomp.ufam.edu.br>
2019-10-03 00:10:47 -0400

    Api rest finished

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
