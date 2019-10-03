cdfe51d803ea2559de1d273a2baee14062e5a3b9
João <pintojr@icomp.ufam.edu.br>
2019-10-02 22:33:07 -0400

    Configure connection with database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 
+const mongoose = require('mongoose');
+
+
 const app = express();
+mongoose.connect('mongodb+srv://aircnc:aircnc@aircnc-eimhg.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
 
 app.use(express.json());
 app.use(routes);
