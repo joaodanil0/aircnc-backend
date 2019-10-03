c1ef5db7626a2f3cb281fcdc95e26cec2c6f0a9d
João <pintojr@icomp.ufam.edu.br>
2019-10-02 23:30:37 -0400

    Added Spot feature to application

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const UsersSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     email: String
 });
 
-module.exports = mongoose.model('User', UsersSchema);
+module.exports = mongoose.model('User', UserSchema);
